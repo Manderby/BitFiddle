81508eb7a26d0265f6db66f887bc30108ca0ac10
Tobias Stamm <manderby@manderby.com>
2019-10-26 22:52:09 +0200

    Interim commit

--- src/BitFiddleNSApplication.m
+++ src/BitFiddleNSApplication.m
@@ -11,6 +11,7 @@
 #include "PreferencesController.h"
 #include "ManderAppAbout.h"
 #include "BitFiddleApplication.h"
+#include "ManderApp.h"
 
 
 @implementation BitFiddleNSApplication
@@ -61,3 +62,31 @@
 }
 
 @end
+
+
+
+void prestartup(void* arg){
+  NA_UNUSED(arg);
+  mandInitManderApp();
+  bitStartApplication();
+}
+
+
+
+void poststartup(void* arg){
+  NA_UNUSED(arg);
+  mandCreateUI();
+  [NSBundle loadNibNamed:@"MainMenu" owner:NSApp];
+  bitCreateUI();
+}
+
+
+
+int main(int argc, char *argv[]){
+  NA_UNUSED(argc);
+  NA_UNUSED(argv);
+  naStartRuntime();
+  [BitFiddleNSApplication sharedApplication];
+  naStartApplication(prestartup, poststartup, NA_NULL);
+  return 0;
+}
